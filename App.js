2cbe93ce1cb151abb3e88cbb76859338fffd7249
Fellipe <fellipealbert3@gmail.com>
2024-02-02 16:16:21 -0300

    add timer

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  const[timer, setTimer]=useState(0)
+  const [timerText, setTimerText]=useState("INICIAR")
+  const [timerInterval, setTimerInterval] =useState(null)
+  const [lastTimerRecord, setLastTimerRecord] =useState(null)
+
+  const handleClearTimer = ()=>{
+    clearInterval(timerInterval);
+    setTimer(0)
+    setTimerText("INICIAR")
+    setLastTimerRecord(timer)
+  }
+
+  const handleTimer = ()=>{
+    if(timerText =='PARAR'){
+      clearInterval(timerInterval);
+      setTimerText("VOLTAR")
+      return
+    }
+    setTimerText("PARAR")
+    setTimerInterval(setInterval(() => {
+      setTimer((prevTimer)=> prevTimer + 0.1)
+    }, 100));
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.timerContent}>
+
+        <Image style={styles.image} source={require("./assets/timer.png")}/>
+       
+    
+        <Text style={styles.timerText}>{timer.toFixed(1)}</Text>
+        <View style={styles.buttonsWrapper}>
+        <TouchableOpacity style={styles.buttons} onPress={handleTimer}>
+         <Text style={styles.textButtons}>{timerText}</Text> 
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttons} onPress={handleClearTimer}>
+        <Text style={styles.textButtons}>LIMPAR</Text> 
+        </TouchableOpacity>
+        </View>
+      </View>
+
+      <Text style={styles.textButtons}>{lastTimerRecord ? "Último tempo marcado: " + lastTimerRecord.toFixed(1) + 's' : ""}</Text> 
+     
+     
     </View>
   );
 }
@@ -13,8 +56,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00aeff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image:{
+    width:280,
+    height:320,
+
+  },
+  timerContent:{
+    justifyContent:"center",
+    alignItems:"center",
+  
+   
+
+  },
+  timerText:{
+ color:"#ffff",
+ fontSize:65,
+marginTop:-180
+
+
+},
+buttonsWrapper:{
+  flexDirection:"row",
+},
+buttons:{
+padding:70,
+marginTop:80
+  },
+  textButtons:{
+    color: "#fff",
+    fontSize:20,
+
+  }
+
+
 });
